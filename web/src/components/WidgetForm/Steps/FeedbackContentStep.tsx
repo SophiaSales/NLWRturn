b6e6fbe079ccbb8a08f42c79a1635d484ed26e2a
Sophia Sales <sophiabrenda10@gmail.com>
2022-05-10 16:22:34 -0300

    Aula2 WidgetForm botões camera remove imagem

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -10,6 +12,8 @@ export function FeedbackContentStep({
     feedbackType, 
     onFeedbackRestarRequested
 }: FeedbackContentStepProps){
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     return(
         <>
@@ -19,7 +23,7 @@ export function FeedbackContentStep({
                     className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
                     onClick={onFeedbackRestarRequested}
                 >
-                    <ArrowLeft weight="bold" className="w-4 h-4"/>
+                    {<ArrowLeft weight="bold" className="w-4 h-4"/>}
                 </button>
                 <span className="text-xl leading-6 flex items-center gap-2">
                     <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
@@ -33,12 +37,10 @@ export function FeedbackContentStep({
                     placeholder="Conte com detalhes o que esta acontecendo... "/>
                 
                 <footer className="flex gap-2 mt-2">
-                    <button
-                        type="button"
-                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-                    >
-                        <Camera className="w-6 h-6"/>
-                    </button>
+                    <ScreenshotButton
+                        screenshot={screenshot}
+                        onScreenshotTook={setScreenshot}
+                    />
                     <button
                         type="submit"
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
