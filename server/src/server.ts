7c8a9839d0aad2a2eef952d71ffa1e4742a5efc8
Sophia Sales <sophiabrenda10@gmail.com>
2022-05-12 15:39:15 -0300

    Aula3 back-end nodemailer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,43 @@
+import { prisma } from "./prisma";
+import nodemailer from 'nodemailer'
 import express from "express";
 
 const app = express();
 
-app.get('/users', (req, res) =>{
-    return res.send('Hello World')
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "ce0c2c98ca929a",
+        pass: "4826fff3f59da5"
+    }
+});
+
+app.post('/feedbacks', async (req, res) =>{
+    const{type, comment, screenshot} = req.body;//retornado valores da tabela do bd
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    })
+    await transport.sendMail({ //enviando email pra pessoa que for fazer um feedback 
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Sophia <sophiabrenda10@gmail.com>',
+        subject: 'Novo feedback',
+        html:[
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo de feedback: ${type}</p>`,
+            `<p>Comentario: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    })
+    
+    return res.status(201).json({data: feedback});//retornando estatus 201 e valores em json quando valores sao envidos para o bd 
 })
 
 app.listen(3333, () =>{
